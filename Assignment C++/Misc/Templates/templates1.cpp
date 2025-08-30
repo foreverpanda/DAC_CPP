b5951b3bae10eaa1495eea858ca4500295989338
foreverpanda <fp4nd4@gmail.com>
2025-08-30 18:38:05 +0000

    Template done with project and STL

--- Assignment C++/Misc/Templates/templates1.cpp
+++ Assignment C++/Misc/Templates/templates1.cpp
@@ -5,6 +5,7 @@
 // Allow user to create int stack, double stack, or even custom object stack.
 
 #include <iostream>
+#include<queue>
 #include <stack>
 using namespace std;
 template <typename T>
@@ -50,13 +51,95 @@ public:
         }
         cout << "Empty\n";
     }
+
+    // inside getStack
+void printAll() {
+    if (s1.empty()) {
+        cout << "Stack is empty!\n";
+        return;
+    }
+    stack<T> temp = s1;
+    cout << "Stack elements (top to bottom): ";
+    while (!temp.empty()) {
+        cout << temp.top() << " ";
+        temp.pop();
+    }
+    cout << endl;
+}
+
+};
+
+template<typename T>
+class getQueue
+{
+    queue<T> s1;
+
+public:
+    getQueue()
+    {
+        cout << "Empty Queue has been created\nUse 1.push(T x) 2.pop() 3.peek() 4.isEmplty() \n\n";
+    }
+    void peek(){
+        if(s1.empty()){
+            cout<<"Queue is empty";
+        }
+        else{
+            cout<<"Front : "<<s1.front();
+            cout<<endl;
+            cout<<"Back : "<<s1.back();
+            cout<<endl;
+        }
+
+    }
+    void push(T val){
+        s1.push(val);
+        cout<<"Pushed : "<<val<<endl;
+    }
+    void pop(){
+        if(s1.empty()){
+            cout<<"Queue is empty";
+
+        }
+        else{
+            cout<<"Popped : "<<s1.front();
+            s1.pop();
+        }
+    }
+        void isEmpty()
+    {
+        if (!s1.empty())
+        {
+            cout << "Not empty\n";
+            return;
+        }
+        cout << "Empty\n";
+    }
+    // inside getQueue
+void printAll() {
+    if (s1.empty()) {
+        cout << "Queue is empty!\n";
+        return;
+    }
+    queue<T> temp = s1;
+    cout << "Queue elements (front to back): ";
+    while (!temp.empty()) {
+        cout << temp.front() << " ";
+        temp.pop();
+    }
+    cout << endl;
+}
+
+   
 };
 
 
-template<typename T, typename container>
+
+
+
+template<typename T, template<typename> typename C>
 void runMenu(){
-    container<T> obj;
-    int choice, val;
+    C<T>obj;
+    int choice; T val;
     do
     {
         cout << "\n📌 Menu\n";
@@ -65,6 +148,8 @@ void runMenu(){
         cout << "3. Peek\n";
         cout << "4. Check Empty\n";
         cout << "5. Exit\n";
+                cout << "6. Print All\n";   // menu line
+
         cout << "Enter choice: ";
         cin >> choice;
 
@@ -87,6 +172,12 @@ void runMenu(){
         case 5:
             cout << "Exiting...\n";
             break;
+
+// switch ke andar
+case 6:
+    obj.printAll();
+    break;
+
         default:
             cout << "Invalid choice! ❌\n";
         }
@@ -110,6 +201,18 @@ int main()
         case 1:
             runMenu<int,getStack>();
             break;
+        case 2:
+            runMenu<double,getStack>();
+            break;
+        case 3:
+        runMenu<int,getQueue>();
+        break;
+        case 4:
+        runMenu<double,getQueue>();
+        break;
+        }
 
     } while (choice != 5);
+
+
 }
